d321317e337091bbee7e039382e073a4e25755f7
GraceW01 <59883422+GraceW01@users.noreply.github.com>
2020-04-17 12:05:35 -0500

    Keycards, guns, jetpacks, oh my

---
+++ Coldboy V6 other assets/Door/doorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour
+{
+    public Animator door;
+    public Inventory player;
+    public string itemName;
+    public BoxCollider2D doorCollision;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            if (player.checkInvetoryFor(itemName))
+            {
+                door.SetBool("isOpen", true);
+                doorCollision.enabled = false;
+            }
+        }
+    }
+}
---
+++ Coldboy V6 other assets/Keycard/collectableScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableScript : MonoBehaviour
+{
+    public string itemName;
+    public Inventory player;
+    // Update is called once per frame
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            player.addToInventory(itemName);
+            Destroy(this.gameObject);
+        }
+    }
+}
