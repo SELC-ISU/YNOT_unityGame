8abd16c9446f3cb2586e0ec1815ca2d4f9e61357
GraceW01 <59883422+GraceW01@users.noreply.github.com>
2020-04-17 12:11:51 -0500

    Sorry I forgot to add the gun
    Its here now

---
+++ Coldboy V6 other assets/Volt Gun/collectGun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectGun : MonoBehaviour
+{
+    public Inventory player;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == ("Player"))
+        {
+            player.gunCollect();
+            Destroy(this.gameObject);
+        }
+    }
+}
