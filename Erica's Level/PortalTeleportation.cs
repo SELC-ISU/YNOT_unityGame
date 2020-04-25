97ddbf5fd422b73feb1b96811d84595b5b7b69b0
ericahollander1 <55969446+ericahollander1@users.noreply.github.com>
2020-04-25 11:30:29 -0500

    My Level With Main Menu

---
+++ Erica's Level/PortalTeleportation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleportation : MonoBehaviour
+{
+    private bool atPortal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        atPortal = false;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+
+        if (Other.tag == "Player")
+        {
+            atPortal = true;
+
+        }
+    }
+    void OnTriggerStay2D(Collider2D Other)
+    {
+        if (atPortal)
+        {
+
+            Other.transform.position = transform.GetChild(0).position;
+            atPortal = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D Other)
+    {
+        atPortal = false;
+    }
+}
